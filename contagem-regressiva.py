6e562937f18ff8963ae1d4e033d177eed63ae885
Bianca <144060187+bianc-x@users.noreply.github.com>
2024-04-06 23:13:40 -0300

    Add files via upload

--- contagem-regressiva.py
+++ contagem-regressiva.py
@@ -1,3 +1,15 @@
 import time
 
-def contagem
+def contagem_regressiva():
+    while t:
+        mins, segs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, segs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    print('O tempo acabou!')
+
+t = int(input(f'Digite o tempo em segundos: '))
+
+contagem_regressiva(t)
